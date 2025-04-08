0f18d4ca371fe6a3395f084770232a89ce7a368e
Kevin Marques <marques.developpement@gmail.com>
2025-04-08 14:21:20 +0200

    fix: type-check storybook

--- src/modules/authentication/components/signup-form/__stories__/signup-form.stories.tsx
+++ src/modules/authentication/components/signup-form/__stories__/signup-form.stories.tsx
@@ -1,14 +1,13 @@
-import * as React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
-import { SignupTemplate } from "../signup-form";
+import { Wrapper as SignupForm } from "../signup-form";
 
 const meta = {
   title: "Templates/Signup",
-  component: SignupTemplate,
+  component: SignupForm,
   parameters: {
     layout: "fullscreen",
   },
-} satisfies Meta<typeof SignupTemplate>;
+} satisfies Meta<typeof SignupForm>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
@@ -17,37 +16,5 @@ export const Default: Story = {
   args: {
     onSubmit: () => {},
     onGoogleButtonClick: () => {},
-    confirmPasswordLabel: "Confirm password",
-    title: "Create an account",
-    description: "Create an account with your email and password",
-    separatorText: "Or continue with",
-    googleButtonText: "Sign up with Google",
-    emailLabel: "Email",
-    passwordLabel: "Password",
-    loginButtonText: "Sign up",
-    invalidEmailText: "Invalid email",
-    invalidPasswordText: "Invalid password",
-    confirmPasswordNotMatchError: "Passwords must match",
-    acceptTermsText: (
-      <>
-        By clicking continue, you agree to our{" "}
-        <a href="#" className="underline">
-          Terms of Service
-        </a>{" "}
-        and{" "}
-        <a href="#" className="underline">
-          Privacy Policy
-        </a>
-        .
-      </>
-    ),
-    signupText: (
-      <>
-        Already have an account?{" "}
-        <a href="#" className="underline">
-          Sign in
-        </a>
-      </>
-    ),
   },
 };
